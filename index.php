6470b734ddac4e6533c9bf8eacbf8ed394c8c80c
Carly Ho <yaysunshine@gmail.com>
2012-04-27 23:18:11 -0500

    Add initial files woop woop

---
+++ index.php
@@ -0,0 +1,21 @@
+<html lang="en">
+	<head>
+	
+	</head>
+	<body>
+		<div id="page">
+			<ul id="nav">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Page 1</a></li>
+				<li><a href="#">Page 2</a></li>
+				<li><a href="#">Page 3</a></li>
+				<li><a href="#">Page 4</a></li>
+				<li><a href="#">Page 5</a></li>
+			</ul>
+			<div id="header">
+				<h1>Site Name Here</h1>
+			</div>
+		</div>
+	</body>
+</html>
